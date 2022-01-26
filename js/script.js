6ca03255ddb6fa60aee6372599ca050c72fc0572
Chris Waters <floridarealty001@gmail.com>
2022-01-26 15:57:00 -0500

    pulling in object data

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
- // make a request to the url
+// make a request to the url
 
- var tallahassee = document.getElementById('tallahassee');
- var cityName = document.getElementById('cityname');
- var submitBtnEl = document.getElementById('submitBtn');
+var tallahassee = document.getElementById('tallahassee');
+var cityName = document.getElementById('cityname');
+var submitBtnEl = document.getElementById('submitBtn');
+var weatherContainer = document.getElementById('weather-container');
+var citySearch = document.getElementById('city-search-term');
+
 
 var apiKey = "e66db7319dc01000529ad3640c6a3281";
 
-var getWeather = function(city) {
+var getWeather = function (city) {
     // 
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-     
+
     fetch(apiUrl).then(function (response) {
         // request was success
         if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data)
+            response.json().then(function (city) {
+                displayWeather(city);
+              //  console.log(data)
             });
         } else {
             alert("No Weather found");
         }
     })
-    };
+        .catch(function (error) {
 
+        })
+};
+
+/// takes city from the form and passes into getWeather function
 var formCityName = function (event) {
     event.preventDefault();
     var city = cityName.value.trim()
@@ -32,10 +40,19 @@ var formCityName = function (event) {
     } else {
         alert("Please enter a City to search")
     }
-  
+
+}
+
+var displayWeather = function(city) {
+    weatherContainer.textContent = "";
+    citySearch.textContent = city.name;
+    citySearch.textContent = city.main.temp;
+    console.log(city)
+// loop over city
+
+
 }
 
 
-
-
+// listens for a click on the form
 submitBtnEl.addEventListener('click', formCityName);
