c710323f001af7ce2b1fbf0084a110ade78bc515
Chris Waters <floridarealty001@gmail.com>
2022-01-26 14:21:39 -0500

    fetch working with submit form

--- js/script.js
+++ js/script.js
@@ -6,10 +6,24 @@
 
 var apiKey = "e66db7319dc01000529ad3640c6a3281";
 
+var getWeather = function(city) {
+    // 
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+     
+    fetch(apiUrl).then(function (response) {
+        // request was success
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)
+            });
+        } else {
+            alert("No Weather found");
+        }
+    })
+    };
 
 var formCityName = function (event) {
     event.preventDefault();
-
     var city = cityName.value.trim()
 
     if (city) {
@@ -18,29 +32,10 @@ var formCityName = function (event) {
     } else {
         alert("Please enter a City to search")
     }
-    console.log(event);
+  
 }
 
 
-function getWeather() {
-// 
-var apiUrl = "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=50&lon=50&appid=e66db7319dc01000529ad3640c6a3281"
- 
-fetch(apiUrl).then(function (response) {
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-})
-    // request was success
-    if (response.ok) {
-        response.json().then(function (data) {
-            displayRepos(data, user);
-        });
-    } else {
-        alert("No Weather found");
-    }
 
-};
 
-submitBtnEl.addEventListener('submit', formCityName);
+submitBtnEl.addEventListener('click', formCityName);
