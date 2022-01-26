f23f37958e6527c4c2cd23778fdf47378681eee5
Chris Waters <floridarealty001@gmail.com>
2022-01-26 13:58:43 -0500

    updated files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
  // make a request to the url
- fetch(apiUrl).then(function (response) {
-    // request was success ful
+
+ var tallahassee = document.getElementById('tallahassee');
+ var cityName = document.getElementById('cityname');
+ var submitBtnEl = document.getElementById('submitBtn');
+
+var apiKey = "e66db7319dc01000529ad3640c6a3281";
+
+
+var formCityName = function (event) {
+    event.preventDefault();
+
+    var city = cityName.value.trim()
+
+    if (city) {
+        getWeather(city);
+        cityName.value = "";
+    } else {
+        alert("Please enter a City to search")
+    }
+    console.log(event);
+}
+
+
+function getWeather() {
+// 
+var apiUrl = "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=50&lon=50&appid=e66db7319dc01000529ad3640c6a3281"
+ 
+fetch(apiUrl).then(function (response) {
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+})
+    // request was success
     if (response.ok) {
         response.json().then(function (data) {
             displayRepos(data, user);
         });
     } else {
-        alert("Error: Github user Not Found");
+        alert("No Weather found");
     }
-})
-    
+
+};
+
+submitBtnEl.addEventListener('submit', formCityName);
