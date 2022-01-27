8c5392d1cad75a27eec27b061c7f5cad8eeb9f7c
Chris Waters <floridarealty001@gmail.com>
2022-01-27 11:59:02 -0500

    added daily forecast

--- js/script.js
+++ js/script.js
@@ -12,20 +12,22 @@ var image = document.getElementById('image');
 
 var currentMoment =  moment().format('dddd - MMM Do YY');
 
-
+var lattitude;
+var longitude;
 
 var apiKey = "e66db7319dc01000529ad3640c6a3281";
 
-var getWeather = function (city) {
+var getWeather = function(city) {
     // 
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
     fetch(apiUrl).then(function (response) {
         // request was success
         if (response.ok) {
-            response.json().then(function (city) {
+            response.json().then(function(city) {
+             //  console.log(response)
                 displayWeather(city);
-              //  console.log(data)
+             
             });
         } else {
             alert("No Weather found");
@@ -40,7 +42,6 @@ var getWeather = function (city) {
 var formCityName = function (event) {
     event.preventDefault();
     var city = cityName.value.trim()
-
     if (city) {
         getWeather(city);
         cityName.value = "";
@@ -51,35 +52,54 @@ var formCityName = function (event) {
 }
 
 var displayWeather = function(city) {
-    iconImage = "https://openweathermap.org/img/wn/" + city['list'][0]['weather'][0]['icon']+".png";
+    console.log(city)
+   
+   var iconImage = document.createElement('img')
+   $("#image").append(iconImage)
+   iconImage.src = "https://openweathermap.org/img/wn/" + city.list[0].weather[0].icon +"@2x.png"
+
+   var temperature = document.createElement('p')
+   temperature.textContent =  "Temperature:" + city.list[0].main.temp
+  var humidity = document.createElement('p')
+  humidity.textContent = "Humity" + city.list[0].main.humidity
+  var windSpeed = document.createElement('p')
+  windSpeed.textContent = "Humity" + city.list[0].wind.speed
+  var groundLevel = document.createElement('p')
+  groundLevel.textContent = "UV" + city.list[0].main.grnd_level
 
 
-    let currentWeather = `
-    <h3>${city.city.name} ${currentMoment}<img src="${iconImage}"></h3>
-    <ul class="list-unstyled">
-        <li>Temperature: ${city.city.name}&#8457;</li>
-        <li>Humidity: ${city.city.sunrise}%</li>
-        <li>Wind Speed: ${city.city.sunrise} mph</li>
-        <li id="uvIndex">UV Index:</li>
-    </ul>`;
+  $(".list-weather").append(temperature, humidity, windSpeed, groundLevel )
+
+
+  
+    
+
+    // let currentWeather = `
+    // <h3>${city.city.name} ${currentMoment}<img src="${iconImage}"></h3>
+    // <ul class="list-unstyled">
+    //     <li>Temperature: ${city.city.name}&#8457;</li>
+    //     <li>Humidity: ${city.city.sunrise}%</li>
+    //     <li>Wind Speed: ${city.city.sunrise} mph</li>
+    //     <li id="uvIndex">UV Index:</li>
+    // </ul>`
   
 
-    weatherContainer = document.createElement("div");
-    weatherContainer.textContent = currentWeather;
+   // weatherContainer = document.createElement("div");
+   // weatherContainer.textContent = currentWeather;
 
 
    
   //  citySearch.textContent = city.main.temp;
- image.textContent = iconImage
- image.textContent = currentWeather
+//  image.textContent = iconImage
+//  image.textContent = currentWeather
 
 
-citySearch.textContent = city['city']['name'];
- image.setAttribute("src", "https://openweathermap.org/img/w/" + city['list'][0]['weather'][0]['icon'] + ".png")
- image.setAttribute = city['list'][0]['main']['feels_like']
-  weatherContainer.textContent = city['list'][0]['weather']['icon'];
-  weatherContainer.textContent = city['city']['sunrise'];
-  weatherContainer.textContent = city['city']['sunrise'];
+// citySearch.textContent = city['city']['name'];
+//  image.setAttribute("src", "https://openweathermap.org/img/w/" + city['list'][0]['weather'][0]['icon'] + ".png")
+//  image.setAttribute = city['list'][0]['main']['feels_like']
+//   weatherContainer.textContent = city['list'][0]['weather']['icon'];
+//   weatherContainer.textContent = city['city']['sunrise'];
+//   weatherContainer.textContent = city['city']['sunrise'];
  
     
  
@@ -92,7 +112,7 @@ citySearch.textContent = city['city']['name'];
 
    // console.log(weatherCity)
 
-  //  console.log(city)
+   
 // loop over city
 
 
