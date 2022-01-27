f9bb03e41d4c2f0132c70ff13f0f6ff6c3d2b8f4
Chris Waters <floridarealty001@gmail.com>
2022-01-27 11:17:57 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 // make a request to the url
 
+
+
 var tallahassee = document.getElementById('tallahassee');
 var cityName = document.getElementById('cityname');
 var submitBtnEl = document.getElementById('submitBtn');
 var weatherContainer = document.getElementById('weather-container');
 var citySearch = document.getElementById('city-search-term');
+var image = document.getElementById('image');
+
+
+var currentMoment =  moment().format('dddd - MMM Do YY');
+
 
 
 var apiKey = "e66db7319dc01000529ad3640c6a3281";
 
 var getWeather = function (city) {
     // 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
     fetch(apiUrl).then(function (response) {
         // request was success
@@ -44,10 +51,48 @@ var formCityName = function (event) {
 }
 
 var displayWeather = function(city) {
-    weatherContainer.textContent = "";
-    citySearch.textContent = city.name;
-    citySearch.textContent = city.main.temp;
-    console.log(city)
+    iconImage = "https://openweathermap.org/img/wn/" + city['list'][0]['weather'][0]['icon']+".png";
+
+
+    let currentWeather = `
+    <h3>${city.city.name} ${currentMoment}<img src="${iconImage}"></h3>
+    <ul class="list-unstyled">
+        <li>Temperature: ${city.city.name}&#8457;</li>
+        <li>Humidity: ${city.city.sunrise}%</li>
+        <li>Wind Speed: ${city.city.sunrise} mph</li>
+        <li id="uvIndex">UV Index:</li>
+    </ul>`;
+  
+
+    weatherContainer = document.createElement("div");
+    weatherContainer.textContent = currentWeather;
+
+
+   
+  //  citySearch.textContent = city.main.temp;
+ image.textContent = iconImage
+ image.textContent = currentWeather
+
+
+citySearch.textContent = city['city']['name'];
+ image.setAttribute("src", "https://openweathermap.org/img/w/" + city['list'][0]['weather'][0]['icon'] + ".png")
+ image.setAttribute = city['list'][0]['main']['feels_like']
+  weatherContainer.textContent = city['list'][0]['weather']['icon'];
+  weatherContainer.textContent = city['city']['sunrise'];
+  weatherContainer.textContent = city['city']['sunrise'];
+ 
+    
+ 
+
+    // var weatherEl = document.createElement("a");
+    // weatherEl.classList = "list-item flex-row justify-space-between align-center";
+    // weatherEl.setAttribute("href",);
+
+    // weatherEl.appendChild(weatherCity);
+
+   // console.log(weatherCity)
+
+  //  console.log(city)
 // loop over city
 
 
