66f5b5d88fa2502662dd1928c2bed7fbe6991122
Chris Waters <floridarealty001@gmail.com>
2022-01-28 09:57:14 -0500

    updated to vanilla

--- js/script.js
+++ js/script.js
@@ -8,26 +8,28 @@ var submitBtnEl = document.getElementById('submitBtn');
 var weatherContainer = document.getElementById('weather-container');
 var citySearch = document.getElementById('city-search-term');
 var image = document.getElementById('image');
+var listGroupEl = document.querySelector(".list-group")
+var divblockEl = document.getElementById('divblock');
 
 
-var currentMoment =  moment().format('dddd - MMM Do YY');
+var currentMoment = moment().format('dddd - MMM Do YY');
 
 var lattitude;
 var longitude;
 
 var apiKey = "e66db7319dc01000529ad3640c6a3281";
 
-var getWeather = function(city) {
+var getWeather = function (city) {
     // Single day weather info
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
     fetch(apiUrl).then(function (response) {
         // request was success
         if (response.ok) {
-            response.json().then(function(city) {
-             //  console.log(response)
+            response.json().then(function (city) {
+                //  console.log(response)
                 displayWeather(city);
-             
+
             });
         } else {
             alert("No Weather found");
@@ -51,88 +53,100 @@ var formCityName = function (event) {
 
 }
 
-var displayWeather = function(city) {
+var displayWeather = function (city) {
     console.log(city)
-   
-   var iconImage = document.createElement('img')
-   $("#image").append(iconImage)
-   iconImage.src = "https://openweathermap.org/img/wn/" + city.list[0].weather[0].icon +"@4x.png";
 
+   
+    var iconImage = document.createElement('img')
+    $("#image").append(iconImage)
+    iconImage.src = "https://openweathermap.org/img/wn/" + city.list[0].weather[0].icon + "@4x.png";
+
+    
     var cityName = document.createElement('h2')
     cityName.classList = " flex-row align-left";
     cityName.textContent = city.city.name;
-   var iconImage = document.createElement('img')
-   iconImage.src = "https://openweathermap.org/img/wn/" + city.list[0].weather[0].icon +"@4x.png";
-   var temperature = document.createElement('p')
-   temperature.textContent =  "Temperature: " + parseInt((((city.list[0].main.temp) - 273.15) * (9/5)) + 32) + "*F";
-  var humidity = document.createElement('p')
-  humidity.textContent = "Humity: " + city.list[0].main.humidity + "%";
-  var windSpeed = document.createElement('p')
-  windSpeed.textContent = "Wind Speed: " + parseInt(city.list[0].wind.speed) + " m/hr";
-  var population = document.createElement('p')
-  population.textContent = "Population: " + (city.city.population).toLocaleString("en-US") + " People.";
+    var iconImage = document.createElement('img')
+    iconImage.src = "https://openweathermap.org/img/wn/" + city.list[0].weather[0].icon + "@4x.png";
+    var temperature = document.createElement('p')
+    temperature.textContent = "Temperature: " + parseInt((((city.list[0].main.temp) - 273.15) * (9 / 5)) + 32) + "*F";
+    var humidity = document.createElement('p')
+    humidity.textContent = "Humity: " + city.list[0].main.humidity + "%";
+    var windSpeed = document.createElement('p')
+    windSpeed.textContent = "Wind Speed: " + parseInt(city.list[0].wind.speed) + " m/hr";
+    var population = document.createElement('p')
+    population.textContent = "Population: " + (city.city.population).toLocaleString("en-US") + " People.";
 
 
-  $("#city-weather").append(cityName, iconImage, temperature, humidity, windSpeed, population )
+    $("#city-weather").append(cityName, iconImage, temperature, humidity, windSpeed, population)
 
-  var lat = city.city.coord.lat
-  var lon = city.city.coord.lon
+    var lat = city.city.coord.lat
+    var lon = city.city.coord.lon
 
-  var fiveDayApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&appid=" + apiKey;
+    var fiveDayApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
 
-  fetch(fiveDayApi).then(function (response) {
-    // request was success
-    if (response.ok) {
-        response.json().then(function(secondFeed) {
-       // adds UV to top part of weather
-            var uvIndex = document.createElement('p')
-            uvIndex.textContent = "UV Index: " + secondFeed.current.uvi
-            $("#city-weather").append(uvIndex)
-
-          for (var i =0; i < 6; i++) {
+    fetch(fiveDayApi).then(function (response) {
+        // request was success
+        if (response.ok) {
+            response.json().then(function (secondFeed) {
+                // adds UV to top part of weather
+                var uvIndex = document.createElement('p')
+                uvIndex.textContent = "UV Index: " + secondFeed.current.uvi
+                $("#city-weather").append(uvIndex)
 
               
-            fiveDayImage = document.createElement('img')
-               fiveDayImage.src = "https://openweathermap.org/img/wn/" + secondFeed.daily[i].weather[0].icon +".png";
-                 var clouds = document.createElement('p');
-                clouds.textContent = "clouds: " + secondFeed.daily[i].clouds;
-                var date = document.createElement('p')
-                date.textContent = secondFeed.daily[i].dt
-           //     image.src: "https://openweathermap.org/img/wn/" + secondFeed.daily[i].weather[0].icon +".png";
-                console.log(date, clouds, fiveDayImage)
 
-                  $(".list-group").append(fiveDayImage, clouds, date )
-        };
                
-            
-          
-                
-             
-           
-            
-         
+                for (var i = 0; i < 6; i++) {
+              
+                    var divWeather = document.createElement('div')
+
+                var fiveDayImage = document.createElement('img')
+                    fiveDayImage.src = "https://openweathermap.org/img/wn/" + secondFeed.daily[i].weather[0].icon + "@2x.png";
+                    var clouds = document.createElement('p');
+                    clouds.textContent = "clouds: " + secondFeed.daily[i].clouds;
+                    var date = document.createElement('p')
+                    date.textContent = moment.unix(secondFeed.daily[i].dt).format("MM/DD/YYYY");
+                    var fiveDayTempLow = document.createElement('p')
+                    fiveDayTempLow.textContent = "Low Temp: " + parseInt((((secondFeed.daily[i].temp.min) - 273.15) * (9 / 5)) + 32);
+                    var fiveDayTempHigh = document.createElement('p')
+                    fiveDayTempHigh.textContent = "High Temp: " + parseInt((((secondFeed.daily[0].temp.max) - 273.15) * (9 / 5)) + 32)
+
+                  
 
 
-       //  var UvIndex = data.value;
-         console.log(secondFeed)
-            fiveDay(lat, lon);
-         
-        });
-    } else {
-        alert("No Weather found");
-    }
-})
-    .catch(function (error) {
 
+                    listGroupEl.append(divWeather, fiveDayImage, clouds, date, fiveDayTempLow, fiveDayTempHigh)
+                };
+
+
+
+
+
+
+
+
+
+
+                //  var UvIndex = data.value;
+                console.log(secondFeed)
+                fiveDay(lat, lon);
+
+            });
+        } else {
+            alert("No Weather found");
+        }
     })
+        .catch(function (error) {
+
+        })
 
 
 }
 
 
-var fiveDay = function(lat, lon){
-console.log(lat, lon)
+var fiveDay = function (lat, lon) {
+    console.log(lat, lon)
 }
 
 
